9b94d709950f6d0a6b9dbbdef147a27a032fda68
Karthikeyan Rajasekar <reach4karthik27@gmail.com>
2024-10-08 21:28:13 -0500

    Updated valid sudoku code to include space optimized solution.

--- Array_And_Hashing/ValidSudoku.py
+++ Array_And_Hashing/ValidSudoku.py
@@ -1,5 +1,10 @@
 from collections import defaultdict
+from typing import List
 
+''' 
+This solution works but the space complexity can be improved. 
+Space complexity is O(n^2).
+'''
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         cols = defaultdict(dict)
@@ -19,7 +24,46 @@ class Solution:
                         box[(box_r,box_c)][num] = True
 
         return True
+
+''' 
+This solution improves the space complexity by using bit masking using &(and) and |(or) bitwise operators. 
+Space complexity is O(n).
+'''
+class Solution2:
     
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        N = 9
+        cols = [0] * N
+        rows = [0] * N
+        box = [0] * N
+
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                num = board[r][c]
+
+                if num != '.':
+                    num = int(num) - 1
+
+                    #check in column
+                    if cols[c] & (1 << num):
+                        return False
+                    else:
+                        cols[c] = cols[c] | (1 << num)
+
+                    #check in row
+                    if rows[r] & (1 << num):
+                        return False
+                    else:
+                        rows[r] = rows[r] | (1 << num)
+
+                    #check in box
+                    box_index = (r // 3) * 3 + (c // 3)
+                    if box[box_index] & (1 << num):
+                        return False
+                    else:
+                        box[box_index] = box[box_index] | (1 << num)
+
+        return True
 
 board_1 = [["5","3",".",".","7",".",".",".","."]
 ,["6",".",".","1","9","5",".",".","."]
@@ -41,3 +85,13 @@ board_2 = [["8","3",".",".","7",".",".",".","."]
 ,[".","6",".",".",".",".","2","8","."]
 ,[".",".",".","4","1","9",".",".","5"]
 ,[".",".",".",".","8",".",".","7","9"]]
+
+
+sol1 = Solution()
+sol2 = Solution2()
+
+assert sol1.isValidSudoku(board_1) != False, "Assertion fail, this is a valid board"
+assert sol2.isValidSudoku(board_1) != False, "Assertion fail, this is a valid board"
+
+assert sol1.isValidSudoku(board_2) != True, "Assertion fail, this is an invalid board"
+assert sol2.isValidSudoku(board_2) != True,  "Assertion fail, this is an invalid board"
